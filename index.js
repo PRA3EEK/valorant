2716709d96e4acfcbb534dd89798c727e099f6fe
pra3eek <prateekchauhan0004@gmail.com>
2023-02-25 21:29:56 +0530

    commit

--- index.js
+++ index.js
@@ -1,51 +1,49 @@
-fetch('https://valorant-api.com/v1/weapons',{
-    method:'GET',
-   
-    headers: { 
-     "Content-type":"application/json; charset=UTF-8"
-    }
-    
-  })
-  .then(function(response){
+fetch('https://valorant-api.com/v1/weapons', {
+  method: 'GET',
+
+  headers: {
+    "Content-type": "application/json; charset=UTF-8"
+  }
+
+})
+  .then(function (response) {
     return response.json();
   })
-  .then(function(body){
+  .then(function (body) {
     console.log(body);
     display(body);
   })
 
-  let display = (body) => {
+let display = (body) => {
 
-   let data = body.data;
-   let parent = document.getElementById("weapons");
-   for(let i=0 ; i<data.length ; i++)
-   {
+  let data = body.data;
+  let parent = document.getElementById("weapons");
+  for (let i = 0; i < data.length; i++) {
     let div = document.createElement("div");
     div.style.border = "2px solid black";
     let img = document.createElement("img");
     let title = document.createElement("p");
     let descDiv = document.createElement("div");
     let equipTimeSeconds = document.createElement("p");
-    equipTimeSeconds.innerText = 'Equip Time Seconds: '+data[i].weaponStats.equipTimeSeconds;
+    equipTimeSeconds.innerText = 'Equip Time Seconds: ' + data[i].weaponStats.equipTimeSeconds;
     let fireRate = document.createElement("p");
-    fireRate.innerText = 'Fire Rate: '+data[i].weaponStats.fireRate;
+    fireRate.innerText = 'Fire Rate: ' + data[i].weaponStats.fireRate;
     let firstBulletAccuracy = document.createElement("p");
-    firstBulletAccuracy = 'First Bullet Accuracy: '+data[i].weaponStats.firstBulletAccuracy;
+    firstBulletAccuracy = 'First Bullet Accuracy: ' + data[i].weaponStats.firstBulletAccuracy;
     let magazineSize = document.createElement("p");
-    magazineSize.innerText = 'Magazine Size: '+ data[i].weaponStats.magazineSize;
+    magazineSize.innerText = 'Magazine Size: ' + data[i].weaponStats.magazineSize;
     let reloadTimeSeconds = document.createElement("p");
-    reloadTimeSeconds.innerText = 'Reload Time Seconds: '+data[i].weaponStats.reloadTimeSeconds;
+    reloadTimeSeconds.innerText = 'Reload Time Seconds: ' + data[i].weaponStats.reloadTimeSeconds;
     let runSpeedMultiplier = document.createElement("p");
-    runSpeedMultiplier = 'Run Speed Multiplier: '+data[i].weaponStats.runSpeedMultiplier;
+    runSpeedMultiplier = 'Run Speed Multiplier: ' + data[i].weaponStats.runSpeedMultiplier;
     let shotgunPelletCount = document.createElement("p");
-    shotgunPelletCount.innerText = 'Shotgun Pellet Count: '+data[i].weaponStats.shotgunPelletCount;
+    shotgunPelletCount.innerText = 'Shotgun Pellet Count: ' + data[i].weaponStats.shotgunPelletCount;
     let wallPenetration = document.createElement("p");
-    wallPenetration.innerText = 'Wall Penetration: '+data[i].weaponStats.wallPenetration.split(":")[2];
+    wallPenetration.innerText = 'Wall Penetration: ' + data[i].weaponStats.wallPenetration.split(":")[2];
     descDiv.append(equipTimeSeconds, fireRate, firstBulletAccuracy, magazineSize, reloadTimeSeconds, runSpeedMultiplier, shotgunPelletCount, wallPenetration);
     title.innerText = data[i].displayName;
     img.src = data[i].displayIcon;
     div.append(img, title, descDiv);
     parent.appendChild(div);
-   }
-
   }
+}
